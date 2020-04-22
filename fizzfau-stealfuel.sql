1b26523e47507185f5d2801c392b2a8cc24912d6
fizzfau <59974801+fizzfau@users.noreply.github.com>
2020-04-23 00:17:24 +0300

    Update fizzfau-stealfuel.sql

--- fizzfau-stealfuel.sql
+++ fizzfau-stealfuel.sql
@@ -2,7 +2,5 @@ USE `essentialmode`;
 
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
 	
-('hose', 'Hortum', 2, 0, 1)
-,
-('empty_gascan', 'Boþ Benzin Bidonu', 2, 0, 1)
-;
+('hose', 'Hortum', 2, 0, 1),
+('empty_gascan', 'BoÃ¾ Benzin Bidonu', 2, 0, 1);
